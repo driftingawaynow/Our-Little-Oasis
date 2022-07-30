0dea06bb2095db9ef66df835f0872f0a3ed537ca
Chaz630 <36994814+driftingawaynow@users.noreply.github.com>
2022-07-29 20:46:34 -0400

    Initial commit

---
+++ Assets/Scripts/WallJump.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallJump : MonoBehaviour
+{
+    [Header("Walljump")]
+    public LayerMask Wall;
+    public LayerMask Ground;
+    public float wallJumpUpForce;
+    public float wallJumpSideForce;
+
+    [Header("Input")]
+    public KeyCode jumpKey = KeyCode.Space;
+    private float horizontalInput;
+    private float verticalInput;
+
+    [Header("Detection")]
+    public float wallCheckDistance;
+    public float minJumpHeight;
+    private RaycastHit leftWallhit;
+    private RaycastHit rightWallhit;
+    private bool wallLeft;
+    private bool wallRight;
+
+    [Header("Gravity")]
+    public bool useGravity;
+    public float gravityCounterForce;
+
+    [Header("References")]
+    public Transform orientation;
+    public PlayerCam cam;
+    private PlayerMovementAdvanced pm;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        pm = GetComponent<PlayerMovementAdvanced>();
+    }
+
+    private void Update()
+    {
+        CheckForWall();
+        StateMachine();
+    }
+
+    private void FixedUpdate()
+    {
+
+    }
+
+    private void CheckForWall()
+    {
+        wallRight = Physics.Raycast(transform.position, orientation.right, out rightWallhit, wallCheckDistance, Wall);
+        wallLeft = Physics.Raycast(transform.position, -orientation.right, out leftWallhit, wallCheckDistance, Wall);
+    }
+
+    private bool AboveGround()
+    {
+        return !Physics.Raycast(transform.position, Vector3.down, minJumpHeight, Ground);
+    }
+
+    private void StateMachine()
+    {
+        // Getting Inputs
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        // State 1 - Wallrunning
+        if((wallLeft || wallRight) && verticalInput > 0 && AboveGround())
+        {
+            // wall jump
+            if (Input.GetKeyDown(jumpKey)) Wall_Jump();
+        }
+    }
+
+    private void Wall_Jump()
+    {
+        Vector3 wallNormal = wallRight ? rightWallhit.normal : leftWallhit.normal;
+
+        Vector3 forceToApply = transform.up * wallJumpUpForce + wallNormal * wallJumpSideForce;
+
+        // reset y velocity and add force
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+        rb.AddForce(forceToApply, ForceMode.Impulse);
+    }
+}
