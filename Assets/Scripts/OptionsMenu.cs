aeaddb320166af71a6bbf3fe0148e8a5441574eb
Chaz630 <36994814+driftingawaynow@users.noreply.github.com>
2022-08-14 17:26:32 -0400

    Fixing player prefs

--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -2,12 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class OptionsMenu : MonoBehaviour
 {
     List<int> widths = new List<int>() {568, 960, 1280, 1920}; 
     List<int> heights = new List<int>() {320, 540, 800, 1080}; 
+    [SerializeField] Slider sens;
 
+    public void Start()
+    {
+        sens.value = PlayerPrefs.GetFloat("sens");
+    }
     public void Options()
     {
 
--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -15,7 +15,7 @@ public class PauseMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.G))
+        if(Input.GetKeyDown(KeyCode.Escape))
         {
             if(paused)
             {
